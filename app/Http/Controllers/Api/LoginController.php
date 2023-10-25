af641a942988f055d7883e2ff230218d54e58e37
Jean <acalto2@gmail.com>
2023-10-25 19:38:43 +0200

    67. LoginController, access token, Postman test p4

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Carbon;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Exception;
 
@@ -55,10 +55,32 @@ class LoginController extends Controller
             'online'
         )->where($map)->first();
         if (empty($result)) {
+
+            $validated['token'] = md5(uniqid() . rand(10000, 99999));
+            $validated['created_at'] = \Carbon\Carbon::now();
+            $validated['access_token'] = md5(uniqid() . rand(1000000, 9999999));
+            $validated['expire_date'] = Carbon::now()->addDays(30);
+            $user_id = DB::table('users')->insertGetId($validated);
+            $user_result = DB::table('users')->select(
+                'avatar',
+                'name',
+                'description',
+                'type',
+                'token',
+                'access_token',
+                'online'
+            )->where('id', '=', $user_id)->first();
+
             return [
                 'code' => 0,
+                'data' => $user_result,
+                'msg' => 'user has been created'
+            ];
+        } else {
+            return [
+                'code' => 1,
                 'data' => $result,
-                'msg' => 'no user found'
+                'msg' => 'User already exist',
             ];
         }
     }
