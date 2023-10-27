7f5c651489f7528f2ed6b7f2c6f00cc356f3a837
Jean <acalto2@gmail.com>
2023-10-27 19:01:37 +0200

    81. Contact module and middleware part 3

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class CheckUser
 {
@@ -21,7 +23,48 @@ class CheckUser
             );
         }
         $access_token = trim(ltrim($Authorization, 'Bearer'));
+        $res_user = DB::table('users')->where('access_token', $access_token)
+            ->select('id', 'avatar', 'name', 'token', 'access_token', 'expire_date')
+            ->first();
 
+        if (empty($res_user)) {
+            return response(
+                [
+
+                    'code' => 401,
+                    'message' => 'User does not exist',
+                ],
+                401
+            );
+        }
+        $expire_date = $res_user->expire_date;
+        if (empty($expire_date)) {
+            return response(
+                [
+
+                    'code' => 401,
+                    'message' => 'You must login again',
+                ],
+                401
+            );
+        }
+        if ($expire_date < Carbon::now()) {
+            return response(
+                [
+
+                    'code' => 401,
+                    'message' => 'Your token has expired. You must login again',
+                ],
+                401
+            );
+        }
+        $addTime = Carbon::now()->addDays(5);
+        if ($expire_date < $addTime) {
+            $add_expire_date = Carbon::now()->addDays(30);
+            DB::table('users')
+                ->where('access_token', $access_token)
+                ->update(['expire_date' => $add_expire_date]);
+        }
         return $next($request);
     }
 }
